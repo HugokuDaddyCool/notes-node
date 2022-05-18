86fc67bce8cec31b25a22a900934051b8cbb297a
Victor Cortes <victor.cortes@softtek.com>
2022-05-18 15:00:52 -0500

    starting chapter 3

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 console.log("Starting app.js");
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('.\\notes.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString('true'));
+var command = process.argv[2];
+console.log(process.argv);
+console.log('Command: ', command);
 
-var filteredArray = _.uniq([1,2,3,'victor',4,5,'victor']);
-console.log(filteredArray);
-// var res = notes.add(9,-2);
-// console.log(res);
-
-// var user = os.userInfo();
-
-// fs.appendFile('greetings.txt', `Hello ${user.username} and you are ${notes.age} years old!`, (err) => {
-//   if (err) {
-//     console.log('unable to write file!');
-//   }
-// });
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading a note');
+} else if (command === 'remove') {
+    console.log('Removing a note');
+} else {
+    console.log('Command not recognized');
+}
