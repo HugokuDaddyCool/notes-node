5f9b5502a0d818dfc91456384905b85ab57c1e5b
Victor Cortes <victor.cortes@softtek.com>
2022-05-19 10:37:11 -0500

    starting chapter 4

--- notes.js
+++ notes.js
@@ -1,7 +1,21 @@
 console.log('Starting notes.js');
 
+var fs = require('fs');
+
 var addNote = (title, body) => {
-    console.log('Adding note: ', title, body);
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    };
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
 };
 var getAll = () => {
     console.log('Getting all notes');
@@ -12,6 +26,21 @@ var getNote = (title) => {
 var removetNote = (title) => {
     console.log('Removing note with title ', title);
 };
+
+var fetchNotes = () => {
+    try {
+        var notesString = fs.readFileSync('note-data.json');
+        return JSON.parse(notesString);
+    } catch (e) {
+    }
+    return [];
+}
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('note-data.json', JSON.stringify(notes));    
+}
+
+
 
 module.exports = {
     addNote,
