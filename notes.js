7aa6506a52d8d74f48c8588547ceb1f65454fc39
Victor Cortes <victor.cortes@softtek.com>
2022-05-19 09:45:14 -0500

    Starting chapter 3 JSON section

--- notes.js
+++ notes.js
@@ -1,7 +1,21 @@
 console.log('Starting notes.js');
 
-module.exports.age = 46;
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
-}
+var addNote = (title, body) => {
+    console.log('Adding note: ', title, body);
+};
+var getAll = () => {
+    console.log('Getting all notes');
+};
+var getNote = (title) => {
+    console.log('Reading note with title ', title);
+};
+var removetNote = (title) => {
+    console.log('Removing note with title ', title);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removetNote
+};
